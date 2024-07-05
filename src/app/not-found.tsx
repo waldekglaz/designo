e875e43bf6f9ac4d92d4831d1d5d43bcf3d8a158
Waldemar Glaz <waldekglaz@gmail.com>
2024-07-05 16:54:54 +0200

    404 page sketch

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link href="/">Go back to the homepage</Link>
+    </div>
+  );
+};
+
+export default NotFound;

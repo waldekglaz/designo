25026e83ead1f43af27d89bfe23597b974aebe56
Waldemar Glaz <waldekglaz@gmail.com>
2024-07-12 17:28:45 +0200

    MobileNav animation

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Link from "next/link";
 import { menuItems } from "@/constants/const";
+import { motion, spring } from "framer-motion";
 
 interface MobileNavProps {
   isMenuOpen: boolean;
   onClick: () => void;
 }
 
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "-100%" },
+};
+
 const MobileNav = ({ isMenuOpen, onClick }: MobileNavProps) => {
   return (
-    <nav
-      className={`${
-        isMenuOpen ? "block" : "hidden"
-      }   text-white bg-slate-950 bg-opacity-50 absolute top-[75px] left-0 w-full h-full z-50`}
+    <motion.nav
+      initial={false}
+      animate={isMenuOpen ? "open" : "closed"}
+      transition={{ type: spring, stiffness: 260, damping: 20 }}
+      variants={variants}
+      className={`text-white bg-slate-950 bg-opacity-50 absolute top-[75px] left-0 w-full h-full z-50`}
     >
       <ul className=" bg-slate-900 px-4 py-8">
         {menuItems.map((item) => (
@@ -23,7 +31,7 @@ const MobileNav = ({ isMenuOpen, onClick }: MobileNavProps) => {
           </li>
         ))}
       </ul>
-    </nav>
+    </motion.nav>
   );
 };
 
